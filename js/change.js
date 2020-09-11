9284c43460ec7991ef76e2fe119107ee011bd10e
alex95-i <67271327+alex95-i@users.noreply.github.com>
2020-09-11 20:37:09 +0500

    Update change.js

--- js/change.js
+++ js/change.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+$( '#btn1' ).click(function() {
+$('#ing').html('РЕСПУБЛИКА САХА');
+$('#image').attr('src', 'img/Saha.png');
+})
 $( '#btn2' ).click(function() {
 $('#ing').html('ГАЗПРОМ');
 $('#image').attr('src', 'img/football.png');
